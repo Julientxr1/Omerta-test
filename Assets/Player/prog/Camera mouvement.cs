8fab4fb824b5f8c0140be36a4fddcac35c37a94d
Julientxr1 <julien.teixeira@epita.fr>
2023-03-08 19:10:37 +0100

    Revert "changes de camera"
    This reverts commit cf6af751ac8cac0ca944af43ab1ef18e5c078404.

--- Assets/Player/prog/Camera mouvement.cs
+++ Assets/Player/prog/Camera mouvement.cs
@@ -8,36 +8,19 @@ using Cursor = UnityEngine.Cursor;
 
 public class Cameramouvement : MonoBehaviour
 {
-    public float moveSpeed;
-    public float shiftAdditionalSpeed;
-    public float mouseSensitivity;
-    public bool invertMouse;
-    public bool autoLockCursor;
- 
-    private Camera cam;
- 
-    void Awake () {
-        cam = this.gameObject.GetComponent<Camera>();
-        this.gameObject.name = "SpectatorCamera";
-        Cursor.lockState = (autoLockCursor)?CursorLockMode.Locked:CursorLockMode.None;
-    }
-   
-    void Update () {
-        float speed = (moveSpeed + (Input.GetAxis("Fire3") * shiftAdditionalSpeed));
-        this.gameObject.transform.Translate(Vector3.forward * speed * Input.GetAxis("Vertical"));
-        this.gameObject.transform.Translate(Vector3.right * speed * Input.GetAxis("Horizontal"));
-        this.gameObject.transform.Translate(Vector3.up * speed * (Input.GetAxis("Jump") + (Input.GetAxis("Fire1") * -1)));
-        this.gameObject.transform.Rotate(Input.GetAxis("Mouse Y") * mouseSensitivity * ((invertMouse) ? 1 : -1), Input.GetAxis("Mouse X") * mouseSensitivity * ((invertMouse) ? -1 : 1), 0);
-        this.gameObject.transform.localEulerAngles = new Vector3(this.gameObject.transform.localEulerAngles.x, this.gameObject.transform.localEulerAngles.y, 0);
- 
-        if (Cursor.lockState == CursorLockMode.None && Input.GetMouseButtonDown(0))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        else if (Cursor.lockState == CursorLockMode.Locked && Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-        }
-    }
+    public float sensi = 100;
 
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse X")<0)
+        {
+            transform.Rotate(0, -sensi * Time.deltaTime,0);
+
+        }
+        if (Input.GetAxis("Mouse X")>0)
+        {
+            transform.Rotate(0, sensi * Time.deltaTime,0);
+
+        }
+    }
 }
