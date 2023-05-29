4a0565439dd7277c6d4099ebf4a8af5ff8e0334c
Julientxr1 <julien.teixeira@epita.fr>
2023-05-29 08:41:12 +0200

    Mission1 corrige

---
+++ Assets/Mission1/Scripts/Mission1 v2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission1v2 : MonoBehaviour
+{
+    public GameObject[] MissionItems;
+    bool MissionStarted;
+    public string tag1;
+    public GameObject MissionOneTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var item in MissionItems)
+        {
+            item.SetActive(false);
+        }
+        MissionStarted = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MissionStarted)
+        {
+            MissionOne();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == tag1)
+        {
+            MissionStarted = true;
+            MissionOneTxt.SetActive(false);
+        }  
+    }
+
+    void MissionOne()
+    {
+        foreach (var item in MissionItems)
+        {
+            item.SetActive(true);
+        }
+    }
+}
